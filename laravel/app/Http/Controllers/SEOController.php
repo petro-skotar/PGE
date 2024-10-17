16a258a6aa2b46ab7087a1fc2bdfa5a527bf6062
petro-skotar <162284308+petro-skotar@users.noreply.github.com>
2024-10-17 11:38:32 -0700

    Disallow off

--- laravel/app/Http/Controllers/SEOController.php
+++ laravel/app/Http/Controllers/SEOController.php
@@ -11,23 +11,24 @@ class SEOController extends Controller
 		/*$offers 	= 	app(ArticlesController::class)->getOffers();
 		$industries = 	app(ArticlesController::class)->getIndustries();*/
 		$blog = 		app(ArticlesController::class)->getBlog();
-		
+
 		return response()->view('seo.sitemap', [
 			/*'offers' => $offers,
 			'industries' => $industries,*/
 			'blog' => $blog,
 		])->header('Content-Type', 'text/xml');
     }
-	
+
     public function robots_txt(Request $request)
     {
 
 $robots_text ='User-agent: *
-Disallow: /templates/
+Disallow: /';
+/*Disallow: /templates/
 Disallow: /livewire/
 Disallow: /login
-Disallow: /?tag';
-		
+Disallow: /?tag';*/
+
 		return response()->view('seo.robots_txt', [
 			'robots_text' => $robots_text,
 		])->header('Content-Type', 'text/plain');
